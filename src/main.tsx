80404ee10a1c547675e8e6d519f873178ee3f3a0
Kevin He <kevin66he@hotmail.com>
2025-04-10 22:52:36 -0400

    Setup Basic Routing to pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import Home from './routes/Home.tsx'
+import Test2 from "./routes/test/test/Test2.tsx";
+import Test from "./routes/test/Test.tsx";
+import ToolsPage from "./routes/tools/ToolsPage.tsx";
+import CalculatorPage from "./routes/tools/calculator/CalculatorPage.tsx";
 
+/*
+    Documentation help for routing
+    https://reactrouter.com/start/declarative/routing
+ */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+
+              <Route path="test">
+                  <Route index element={<Test />} />
+                  <Route path="test2" element={<Test2 />} />
+              </Route>
+
+              <Route path="tools">
+                  <Route index element={<ToolsPage />} />
+                  <Route path="calculator" element={<CalculatorPage />} />
+              </Route>
+          </Routes>
+
+      </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/routes/test/test/Test2.tsx
@@ -0,0 +1,11 @@
+function Test2() {
+    return (
+        <>
+            <div>
+                <h1>Test 2</h1>
+            </div>
+        </>
+    )
+}
+
+export default Test2
